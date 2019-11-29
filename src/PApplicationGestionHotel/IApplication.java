f1cacf0843d87ac1d812268e327ac87d740616a1
Antony CASTANER <antony.castanerpynov.com>
2019-11-29 15:04:53 +0100

    Add IApplication Interface

---
+++ src/PApplicationGestionHotel/IApplication.java
@@ -0,0 +1,104 @@
+package PApplicationGestionHotel;
+
+public interface IApplication {
+
+    /**
+     * Afficher le menu general de l'application
+     */
+    public void afficherMenu();
+
+    /**
+     * Afficher la totalite des reservations
+     */
+    public void afficherReservations();
+
+    /**
+     * Afficher une reservation specifique
+     */
+    public void afficherReservation();
+
+    /**
+     * Afficher la totalite des chambre dispos pour une periode donnee
+     */
+    public void afficherChambresDispos();
+
+    /**
+     * Afficher le recapitulatif pour l'ajout d'une reservation
+     */
+    public void afficherRecapitulatif();
+
+    /**
+     * Afficher succes d'une operation
+     */
+    public void afficherSucces();
+
+    /**
+     * Demander les dates MAJ d'une reservation
+     */
+    public void demanderDates();
+
+    /**
+     * Demander le client a choisir pour la reservation
+     */
+    public void demanderClient();
+
+    /**
+     * Demander la validation de l'annulation
+     */
+    public void checkAnnulerReservation();
+
+    /**
+     * Annuler une reservation
+     */
+    public void annulerReservation();
+
+    /**
+     * Recuperer les dates entrees
+     */
+    public void recupererDates();
+
+    /**
+     * Recuperer les chambres selectionnees pour une reservation
+     */
+    public void recupererChambresSelectionnees();
+
+    /**
+     * Valider une reservation
+     */
+    public void validerReservation();
+
+    /**
+     * Recuperer le client selectionne
+     */
+    public void recupererClient();
+
+    /**
+     * Afficher la totalite des clients
+     */
+    public void afficherClients();
+
+    /**
+     * Afficher le formulaire pour l'ajout d'un client
+     */
+    public void afficherFormulaireClient();
+
+    /**
+     * Recuperer le formulaire pour l'ajout d'un client
+     */
+    public void recupererFormulaireClient();
+
+    /**
+     * Afficher le formulaire pour la MAJ d'un client
+     */
+    public void afficherFormulaireClientMAJ();
+
+    /**
+     * Recuperer le client a mettre a jour
+     */
+    public void recupererClientMAJ();
+
+    /**
+     * Afficher la totalite des chambres
+     */
+    public void afficherChambres();
+}
