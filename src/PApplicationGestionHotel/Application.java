cbe9a54c329755e89996d1f501be9b582a62af9b
Antony CASTANER <antony.castanerpynov.com>
2019-11-29 15:09:18 +0100

    Add Application class with empty methods

---
+++ src/PApplicationGestionHotel/Application.java
@@ -0,0 +1,164 @@
+package PApplicationGestionHotel;
+
+public class Application implements IApplication {
+
+    /**
+     * Afficher le menu general de l'application
+     */
+    @Override
+    public void afficherMenu() {
+
+    }
+
+    /**
+     * Afficher la totalite des reservations
+     */
+    @Override
+    public void afficherReservations() {
+
+    }
+
+    /**
+     * Afficher une reservation specifique
+     */
+    @Override
+    public void afficherReservation() {
+
+    }
+
+    /**
+     * Afficher la totalite des chambre dispos pour une periode donnee
+     */
+    @Override
+    public void afficherChambresDispos() {
+
+    }
+
+    /**
+     * Afficher le recapitulatif pour l'ajout d'une reservation
+     */
+    @Override
+    public void afficherRecapitulatif() {
+
+    }
+
+    /**
+     * Afficher succes d'une operation
+     */
+    @Override
+    public void afficherSucces() {
+
+    }
+
+    /**
+     * Demander les dates MAJ d'une reservation
+     */
+    @Override
+    public void demanderDates() {
+
+    }
+
+    /**
+     * Demander le client a choisir pour la reservation
+     */
+    @Override
+    public void demanderClient() {
+
+    }
+
+    /**
+     * Demander la validation de l'annulation
+     */
+    @Override
+    public void checkAnnulerReservation() {
+
+    }
+
+    /**
+     * Annuler une reservation
+     */
+    @Override
+    public void annulerReservation() {
+
+    }
+
+    /**
+     * Recuperer les dates entrees
+     */
+    @Override
+    public void recupererDates() {
+
+    }
+
+    /**
+     * Recuperer les chambres selectionnees pour une reservation
+     */
+    @Override
+    public void recupererChambresSelectionnees() {
+
+    }
+
+    /**
+     * Valider une reservation
+     */
+    @Override
+    public void validerReservation() {
+
+    }
+
+    /**
+     * Recuperer le client selectionne
+     */
+    @Override
+    public void recupererClient() {
+
+    }
+
+    /**
+     * Afficher la totalite des clients
+     */
+    @Override
+    public void afficherClients() {
+
+    }
+
+    /**
+     * Afficher le formulaire pour l'ajout d'un client
+     */
+    @Override
+    public void afficherFormulaireClient() {
+
+    }
+
+    /**
+     * Recuperer le formulaire pour l'ajout d'un client
+     */
+    @Override
+    public void recupererFormulaireClient() {
+
+    }
+
+    /**
+     * Afficher le formulaire pour la MAJ d'un client
+     */
+    @Override
+    public void afficherFormulaireClientMAJ() {
+
+    }
+
+    /**
+     * Recuperer le client a mettre a jour
+     */
+    @Override
+    public void recupererClientMAJ() {
+
+    }
+
+    /**
+     * Afficher la totalite des chambres
+     */
+    @Override
+    public void afficherChambres() {
+
+    }
+}
