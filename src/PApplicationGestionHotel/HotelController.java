dd3acf8b4ddcb5eec3d53d844711c0d2ac1b29a4
Antony CASTANER <antony.castanerpynov.com>
2019-12-10 18:03:08 +0100

    Add comments to controller

--- src/PApplicationGestionHotel/HotelController.java
+++ src/PApplicationGestionHotel/HotelController.java
@@ -9,10 +9,22 @@ import java.util.*;
 
 public class HotelController implements IHotelController {
 
+    /**
+     * Modele
+     */
     private HotelModel hotelModel;
 
+    /**
+     * Reservation en cours
+     */
     private Reservation reservation;
 
+    /**
+     * Constructeur de la classe HotelController
+     *
+     * @param hotelModel
+     *          Modele
+     */
     public HotelController(HotelModel hotelModel) {
         this.hotelModel = hotelModel;
         this.reservation = new Reservation();
@@ -20,15 +32,22 @@ public class HotelController implements IHotelController {
 
     /**
      * Recuperer la totalite des reservations
+     *
+     * @return
+     *          Liste des reservations
      */
     public ArrayList<Reservation> recupererReservations() {
         try {
+            // Recuperation des reservations
             ResultSet res = this.hotelModel.recupererReservations();
+            // Creation d'une liste de reservations
             ArrayList<Reservation> reservations = new ArrayList<>();
             int count = 0;
             while (res.next())
             {
+                // Verification de l'id des reservations precedente et en cours
                 if (count > 0 && reservations.get(count - 1).id == res.getInt(1)) {
+                    // Creation d'une nouvelle chambre
                     Chambre chambre = new Chambre(
                             res.getInt(7),
                             res.getString(8),
@@ -39,7 +58,7 @@ public class HotelController implements IHotelController {
                             res.getDouble(11)
                     );
                     reservations.get(count - 1).ajouterChambre(chambre.getName());
-                } else {
+                } else { // Ajout d'une nouvelle reservation
                     Reservation reservation = new Reservation();
                     reservation.ajouterId(res.getInt(1));
                     reservation.ajouterDetails(
@@ -49,6 +68,7 @@ public class HotelController implements IHotelController {
                             res.getDate(5),
                             res.getDate(6)
                     );
+                    // Ajout d'une chambre
                     reservation.ajouterChambre(new Chambre(
                             res.getInt(7),
                             res.getString(8),
@@ -58,6 +78,7 @@ public class HotelController implements IHotelController {
                             res.getDouble(13),
                             res.getDouble(11)
                     ).getName());
+                    // Ajout d'un client
                     reservation.ajouterClient(new Client(
                             res.getInt(14),
                             res.getString(15),
@@ -67,6 +88,7 @@ public class HotelController implements IHotelController {
                             res.getString(19)
                     ).getName());
 
+                    // Ajout de la reservation a la liste
                     reservations.add(reservation);
                     count++;
                 }
@@ -79,13 +101,6 @@ public class HotelController implements IHotelController {
     }
 
     /**
-     * Recuperer une reservation specifique
-     */
-    public void recupererReservation() {
-
-    }
-
-    /**
      * Recuperer la reservation en cours d'enregistrement
      */
     public Reservation recupererReservationEnCours() {
@@ -94,6 +109,13 @@ public class HotelController implements IHotelController {
 
     /**
      * Recuperer les chambres disponibles pour une periode
+     *
+     * @param firstDate
+     *          Date de debut
+     * @param secondDate
+     *          Date de fin
+     * @return
+     *          Liste des chambres disponibles
      */
     public ArrayList<Chambre> recupererChambresDipos(Object firstDate, Object secondDate) {
         this.reservation = new Reservation();
@@ -127,11 +149,17 @@ public class HotelController implements IHotelController {
 
     /**
      * Reserve des chambres pour une reservation
+     *
+     * @param boxes
+     *          Cases à choix
+     * @return
+     *          Succes
      */
     public boolean reserverChambres(JCheckBox[] boxes) {
         int count = 0;
         for (JCheckBox box : boxes) {
             if (box.isSelected()) {
+                // Ajout d'une nouvelle chambre
                 this.reservation.ajouterChambre(box.getText());
                 count++;
             }
@@ -145,10 +173,17 @@ public class HotelController implements IHotelController {
 
     /**
      * Reserver un client pour une reservation
+     *
+     * @param bg
+     *          Groupe de boutons
+     * @return
+     *          Succes
      */
     public boolean reserverClient(ButtonGroup bg) {
+        // Recuperation du client
         String client = bg.getSelection().getActionCommand();
         if (client != null) {
+            // Ajout du client a la reservation
             this.reservation.ajouterClient(client);
             return true;
         } else {
@@ -160,13 +195,16 @@ public class HotelController implements IHotelController {
      * Ajouter une nouvelle reservation complete
      */
     public void ajouterReservation() {
+        // Recuperation de l'id du client
         int client_id = Integer.parseInt(this.reservation.client.split(", ")[0]);
+        // Ajout d'une reservation a la BDD
         int reservation_id = this.hotelModel.ajouterReservation(
                 this.reservation.calculNbJour(),
                 new java.sql.Date(Calendar.getInstance().getTime().getTime()),
                 5,
                 client_id
         );
+        // Ajout une par une de chaque chambre a la BDD
         for (String chambre : this.reservation.chambres) {
             List<String> chambreSplit = Arrays.asList(chambre.split(", "));
             int chambre_id = Integer.parseInt(chambreSplit.get(0));
@@ -176,6 +214,9 @@ public class HotelController implements IHotelController {
 
     /**
      * Annuler une reservation
+     *
+     * @param id
+     *          Id de la reservation
      */
     public boolean annulerReservation(int id) {
         if (this.hotelModel.supprimerReservation(id)) {
@@ -188,34 +229,19 @@ public class HotelController implements IHotelController {
     }
 
     /**
-     * Changer de date pour une reservation
-     */
-    public void changerDate() {
-
-    }
-
-    /**
-     * Verifier la disponibilite d'une chambre pour une periode
-     */
-    public void verifierDisponibilite() {
-
-    }
-
-    /**
-     * Calculer le prix d'une chambre
-     */
-    public void calculerPrix() {
-
-    }
-
-    /**
      * Recuperer la totalite des clients
+     *
+     * @return
+     *          Liste des clients
      */
     public ArrayList<Client> recupererClients() {
         try {
+            // Recuperation des clients
             ResultSet res = this.hotelModel.recupererClients();
+            // Creation d'une liste de clients
             ArrayList<Client> clients = new ArrayList<>();
             while (res.next()) {
+                // Ajout d'un client a la liste
                 clients.add(new Client(res.getInt(1), res.getString(2), res.getString(3), res.getInt(4), res.getString(5), res.getString(6)));
             }
             return clients;
@@ -227,11 +253,29 @@ public class HotelController implements IHotelController {
 
     /**
      * Ajouter un nouveau client
+     *
+     * @param mode
+     *          Mode
+     * @param nom
+     *          Nom du client
+     * @param prenom
+     *          Prenom du client
+     * @param cin
+     *          CIN du client
+     * @param telephone
+     *          Numero de telephone du client
+     * @param cb
+     *          Numero de carte bancaire
+     * @return
+     *          Succes
      */
     public boolean ajouterClient(boolean mode, String nom, String prenom, String cin, String telephone, String cb) {
+        // Verification des valeurs entrees
         if (!nom.equals("") && !prenom.equals("") && !cin.equals("") && !telephone.equals("") && !cb.equals("")) {
+            // Ajout d'un client et recuperation de son id
             int id = this.hotelModel.ajouterClient(nom, prenom, Integer.parseInt(cin), telephone, cb);
             if (id != 0) {
+                // Ajout de l'id a la reservation en cours
                 this.reservation.ajouterClient(id + ", " + nom + " " + prenom + ", " + cin);
                 return true;
             }
@@ -263,12 +307,18 @@ public class HotelController implements IHotelController {
 
     /**
      * Recuperer la totalite des chambres
+     *
+     * @return
+     *          Liste de chambres
      */
     public ArrayList<Chambre> recupererChambres() {
         try {
+            // Recuperation des chambres
             ResultSet res = this.hotelModel.recupererChambres();
+            // Creation d'une liste de chambres
             ArrayList<Chambre> chambres = new ArrayList<>();
             while (res.next()) {
+                // Ajout d'une chambre a la reservation en cours
                 chambres.add(new Chambre(res.getInt(1), res.getString(2), res.getString(6), res.getString(4), res.getDouble(3), res.getDouble(5), res.getDouble(7)));
             }
             return chambres;
